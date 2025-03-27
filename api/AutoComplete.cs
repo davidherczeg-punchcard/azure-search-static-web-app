8286a016112e0f9573e471c4965fb1e7ef95e9e0
David Herczeg <david.herczeg@punchcard.io>
2025-03-27 15:06:31 -0600

    Try autocomplete

---
+++ api/AutoComplete.cs
@@ -0,0 +1,69 @@
+using Azure;
+using Azure.Core.Serialization;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+using WebSearch.Models;
+
+namespace WebSearch.Function
+{
+    public class AutoComplete
+    {
+        private static string searchApiKey = Environment.GetEnvironmentVariable("SearchApiKey", EnvironmentVariableTarget.Process);
+        private static string searchServiceName = Environment.GetEnvironmentVariable("SearchServiceName", EnvironmentVariableTarget.Process);
+        private static string searchIndexName = Environment.GetEnvironmentVariable("SearchIndexName", EnvironmentVariableTarget.Process) ?? "good-books";
+
+        private readonly ILogger<Lookup> _logger;
+
+        public AutoComplete(ILogger<Lookup> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("autocomplete")]
+        public async Task<HttpResponseData> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, 
+            FunctionContext executionContext)
+        {
+            // Get Document Id
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonSerializer.Deserialize<RequestBodySuggest>(requestBody);
+
+            // Azure AI Search 
+            Uri serviceEndpoint = new($"https://{searchServiceName}.search.windows.net/");
+
+            SearchClient searchClient = new(
+
+                serviceEndpoint,
+                searchIndexName,
+                new AzureKeyCredential(searchApiKey)
+            );
+
+            AutoCompleteOptions options = new()
+            {
+                Size = data.Size
+            };
+
+            var autoCompleteResponse = await searchClient.AutoCompleteAsync<BookModel>(data.SearchText, data.SuggesterName, options);
+            
+            // Data to return
+            var searchSuggestions = new Dictionary<string, List<SearchSuggestion<BookModel>>>
+            {
+                ["suggestions"] = autoCompleteResponse.Value.Results.ToList()
+            };
+
+            var response = req.CreateResponse(HttpStatusCode.Found);
+
+            // Serialize data
+            var serializer = new JsonObjectSerializer(
+                new JsonSerializerOptions(JsonSerializerDefaults.Web));
+            await response.WriteAsJsonAsync(searchSuggestions, serializer);
+            
+            return response;
+        }
+    }
+}
